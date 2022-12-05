7cd19a0869304852b87ab677d5c9e03f85fb9ff3
Mike <mike.kocevar@gmail.com>
2022-12-05 22:41:42 +0100

    install script V1

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,24 +1,21 @@
 git clone https://github.com/HDInnovations/UNIT3D-Community-Edition.git www
 cp unit3d/.env www
 
+docker compose stop
+docker container rm $(docker container ps -aq)
 docker compose build
 
-docker run --rm -d --name united-mariadb united_docker-mariadb
-docker run --rm -d -v $(pwd)/www:/var/www/html --name united-php-fpm united_docker-php-fpm
-docker exec -it united-php-fpm composer install
-docker exec -it united-php-fpm php artisan key:generate
-docker exec -it united-php-fpm php artisan migrate --seed
-docker exec -it united-php-fpm php artisan clear:all_cache
+docker run --rm -v $(pwd)/www:/var/www/html --name united-php-fpm united_docker-php-fpm
+docker exec -i united-php-fpm 
 
-docker run --rm -d -v $(pwd)/www:/var/www/html --name united-node united_docker-node
-docker exec -it united-node npm install
-docker exec -it united-node npx mix -p
-
-docker stop united-php-fpm
-docker stop united-mariadb
-docker stop united-node
+docker run --rm -v $(pwd)/www:/var/www/html -w /var/www/html node:16 npm install
+docker run --rm -v $(pwd)/www:/var/www/html -w /var/www/html node:16 npx mix -p
 
 docker container rm $(docker container ps -aq)
 
-docker compose stop
-docker compose up
+docker compose up -d
+
+docker exec -i united-php-fpm php artisan clear:all_cache
+docker exec -i united-php-fpm php artisan key:generate
+docker exec -i united-php-fpm php artisan migrate --seed
+docker exec -i united-php-fpm php artisan clear:all_cache
