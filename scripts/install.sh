84a1385f6063d2b17a4fc84e82c897afc641e2fe
mike <mike.kocevar@gmail.com>
2022-12-12 18:28:21 +0100

    Improving the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,16 +5,16 @@ docker compose stop
 docker container rm $(docker container ps -aq)
 docker compose --project-name united build
 
-docker run --rm -d -v $(pwd)/www:/var/www/html --name united-php-fpm united-php-fpm
-docker exec united-php-fpm composer install
-
+docker run --rm -v $(pwd)/www:/var/www/html -w /var/www/html united-php-fpm composer install
 docker run --rm -v $(pwd)/www:/var/www/html -w /var/www/html node:16 npm install
 docker run --rm -v $(pwd)/www:/var/www/html -w /var/www/html node:16 npx mix -p
 
-docker stop united-php-fpm
 docker container rm $(docker container ps -aq)
 
 docker compose up -d
+
+bash -c "while $(docker exec united-php-fpm /bin/bash -c whoami) -ne root > /dev/null; do echo waiting for united-php-fpm container to start; sleep 3; done;"
+bash -c "while $(docker exec united-mariadb /bin/bash -c whoami) -ne root > /dev/null; do echo waiting for united-php-fpm container to start; sleep 3; done;"
 
 docker exec united-php-fpm php artisan clear:all_cache
 docker exec united-php-fpm php artisan key:generate
